128bbec1fa5ed8ffce3f5bdfc75919ff6a03c800
Jose Contreras Stoltze <jmcontreras.stoltze@gmail.com>
2024-04-10 15:57:07 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proyecto_retail_off',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
